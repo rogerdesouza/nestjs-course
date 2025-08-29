8f7b234441124c527907b11121161127dc6be48d
roger <roger@policiacivil.pe.gov.br>
2025-08-29 14:18:11 -0300

    put/patch - delete

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
@@ -9,6 +9,7 @@ export class RecadosController {
   }
 
   // encontrar um recado
+  // @HttpCode(HttpStatus.NOT_FOUND) // mudar o status code do http
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Retorna um recado com id: ${id}`;
@@ -18,5 +19,20 @@ export class RecadosController {
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  // update um novo recado // pode ser patch (partes ou todo) ou put (todo)
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body
+    }
+  }
+
+  // delete um novo recado
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `Deletando o recado com id: ${id}`;
   }
 }
